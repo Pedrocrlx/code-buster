f6b55eb249195b3bb6f9aec5748fbd4ece26ea8a
Pedro Santos <pedrosantoscrlx@gmail.com>
2026-05-20 20:49:44 +0000

    feat(CLI): DB command to create SQLite3 database inside of .buster dir

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from http.client import HTTPException
+
 import typer
+import sqlite3
 from pathlib import Path
 
 app = typer.Typer(help="Code Buster CLI")
 
 
-@app.command()
+@app.command(name="setup")
 def init(
     is_global: bool = typer.Option(
         False, "--global", "-g", help="Create directory on user home (~/)"
@@ -42,6 +45,43 @@ def init(
 
     except Exception as e:
         typer.secho(f"Error creating folder: {e}", fg=typer.colors.RED, err=True)
+
+
+@app.command(name="db")
+def create_db():
+    """
+    Create the SQLite database file in the .buster directory.
+    """
+    target_dir = Path.cwd() / ".buster"
+    db_path = target_dir / "buster.db"
+
+    if not target_dir.exists():
+        typer.secho(
+            "Please run 'init' command first.",
+            fg=typer.colors.RED,
+            err=True,
+        )
+        return
+
+    if db_path.exists():
+        typer.secho(
+            f"Database already exists at {db_path}",
+            fg=typer.colors.YELLOW,
+        )
+        return
+
+    try:
+        conn = sqlite3.connect(db_path)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE projects(name TEXT, topic TEXT, output TEXT)")
+        cur.fetchone()
+        conn.close()
+        typer.secho(
+            f"Success: Database created at {db_path}",
+            fg=typer.colors.GREEN,
+        )
+    except HTTPException as e:
+        typer.secho(f"Error creating database: {e}", fg=typer.colors.RED, err=True)
 
 
 if __name__ == "__main__":
